1a7b3ebff08854873f6052ed6cf97e4800aec1d4
Waruggy <48106241+Waruggy@users.noreply.github.com>
2019-09-20 11:08:49 -0700

    Update BubblePage.js

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,32 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import axiosWithAuth from "../utils/AxiosWithAuth";
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
-
-const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-     useEffect(() =>{
-    axiosWithAuth()
-    .get('http://localhost:5000/api/colors')
-    .then(res => {
-      console.log(res.data);
-      setColorList(res.data)
-    })
-    .catch(e => console.log(e));
-  }, [])
-
-  return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
-  );
-};
-
-export default BubblePage;
-import React, { useState, useEffect } from "react";
-import axiosWithAuth from "../utilities/AxiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
