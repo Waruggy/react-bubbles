b0393206ffa2da537b70200632565c617de44d3b
Waruggy <48106241+Waruggy@users.noreply.github.com>
2019-09-20 11:05:13 -0700

    Update AxiosWithAuth.js

--- client/src/components/utils/AxiosWithAuth.js
+++ client/src/components/utils/AxiosWithAuth.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-  export default function axiosWithAuth(){
-    const token = localStorage.getItem('token');
-
-      return axios.create({
+const axiosWithAuth = () => {
+    return axios.create({
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `${token}`,
-        },
+            Authorization: localStorage.getItem('token')
+        }
     });
-} 
+};
+
+export default axiosWithAuth;
