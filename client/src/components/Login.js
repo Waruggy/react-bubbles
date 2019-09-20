f49874330b5868744164e0cfd27f04471049c375
Waruggy <48106241+Waruggy@users.noreply.github.com>
2019-09-20 11:12:16 -0700

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,36 +1,39 @@
 import React, { useState } from "react";
-import axios from "axios";
-
-const initialState = {
-    username:'Lambda School',
-    password:'i<3Lamb4'
-}
+import axios from 'axios';
 
 const Login = (props) => {
-    const[login, setLogin] = useState(initialState);
-    
-    const handleChange = (e) => {
-    setLogin({...login, [e.target.name]: e.target.value})
-  }
+  
+  const handleChange = event => {
+      setCreds({...creds, [event.target.name]: event.target.value});
+  };
 
-const handleSubmit = e => {
-    e.preventDefault();
-    axios.post('http://localhost:5000/api/login', login)
-            .then(res => {
-                console.log(res)
-                localStorage.setItem('token', res.data.payload);
-                props.history.push('/bubbles')
-            })
-            .catch(err => console.log(err))
-  }
-
+  const handleSubmit = event => {
+      event.preventDefault();
+      axios.post('http://localhost:5000/api/login', creds)
+          .then(res => {
+              console.log(res);
+              localStorage.setItem('token', res.data.payload)
+              props.history.push("/BubblePage");
+          })
+          .catch(err => console.group(err.response));
+  };
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <form onSubmit={handleSubmit}>
-            <input type='text' name ='username' onChange={handleChange} value={login.username} placeholder='Name'></input>
-            <input type='password' name ='password' onChange={handleChange} value={login.password} placeholder='Password'></input>
-            <button>Log In</button>
+      <p>Build a login page here</p>
+      
+       <form onSubmit={handleSubmit}>
+          <input type="type"
+                name="username"
+                placeholder="Username"
+                onChange={handleChange}
+                value={creds.username} />
+          <input type="password"
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+                value={creds.password} />
+          <button type="submit">Log In</button>
       </form>
     </>
   );
